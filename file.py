4dd3abd81ca2e09c14329b4318340acae05df63e
Ratchet1664 <76541198+Ratchet1664@users.noreply.github.com>
2022-02-20 15:29:49 +0000

    Update file.py

--- file.py
+++ file.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 ad_clicks = pd.read_csv('ad_clicks.csv')
 #examines first few rows
-#print(ad_clicks.head())
+print(ad_clicks.head())
 
 #groups utm_source via rows and shows the total clicks
 ad_platform = ad_clicks.groupby(["utm_source"]).user_id.count().reset_index()
@@ -35,5 +35,58 @@ ad_clicks_pivot = ad_clicks_group.pivot(
 
 ad_clicks_pivot["percent_clicked"] = ad_clicks_pivot.apply(
   lambda row: row[True] / (row[False] + row[True]),axis=1)
-
 print(ad_clicks_pivot)
+
+#grouping information to view with greater ease
+add_comparison = ad_clicks.groupby(['experimental_group']).user_id.count().reset_index()
+#print(add_comparison)
+
+percentage = ad_clicks.groupby(["experimental_group","is_click"]).user_id.count().reset_index()
+
+percentage_pivot = percentage.pivot(
+  columns='experimental_group',
+  values='user_id',
+  index='is_click'
+).reset_index()
+
+print(percentage_pivot)
+
+
+a_clicks = ad_clicks[
+   ad_clicks.experimental_group
+   == 'A']
+print(a_clicks)
+
+b_clicks = ad_clicks[
+   ad_clicks.experimental_group
+   == 'B']
+print(a_clicks)
+
+a_clicks_pivot = a_clicks\
+  .groupby(['is_click','day'])\
+  .user_id.count()\
+  .reset_index()\
+  .pivot(
+    index='day',
+    columns='is_click',
+    values='user_id')\
+  .reset_index()
+
+a_clicks_pivot['percentage'] = a_clicks_pivot[True] / \
+(a_clicks_pivot[True] + a_clicks_pivot[False])
+
+b_clicks_pivot = b_clicks\
+  .groupby(['is_click','day'])\
+  .user_id.count()\
+  .reset_index()\
+  .pivot(
+    index='day',
+    columns='is_click',
+    values='user_id')\
+  .reset_index()
+
+b_clicks_pivot['percentage'] = b_clicks_pivot[True] / \
+(b_clicks_pivot[True] + b_clicks_pivot[False])
+
+print(a_clicks_pivot)
+
